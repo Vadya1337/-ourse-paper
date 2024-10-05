90155c73e13e61dc2752573b6acd8a8044ab27ca
Вадя <vadim.filatov.02@gmail.com>
2024-10-06 01:29:44 +0700

    Сделал курсовую на базовую сложность

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,56 @@
+import java.util.Objects;
+
 public class Employee {
+    private final String fullName;
+    private int departament;
+    private int salary;
+    private final int  id;
+public static int count = 1;
+    public Employee(String fullName, int departament, int salary) {
+        this.fullName = fullName;
+        this.departament = departament;
+        this.salary = salary;
+        this.id = count++;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartament() {
+        return departament;
+    }
+
+    public void setDepartament(int departament) {
+        this.departament = departament;
+    }
+
+    public int getSalary() {
+        return salary;
+
+    }
+    @Override
+    public String toString() {
+        return "id=" + id + ", " + "ФИО: "+ fullName + ", Отдел: " + departament + ", Заработная плата: " + salary ;
+    }
+
+public void setSalary(int salary){
+        this.salary = salary;
+}
+    public int getId() {
+        return this.id;
+    }
+    @Override
+    public boolean equals(Object object) {
+        if (this.getClass() != object.getClass()) {
+            return false;
+        }
+        Employee other = (Employee) object ;
+        return fullName.equals(other.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(fullName);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,72 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    private final static Employee[] employees = new Employee[10];
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    private static void initEmpoyees() {
+        employees[0] = new Employee("Некифоров Некита Анатольевич ", 2, 12000);
+        employees[1] = new Employee("Серебряков Анатолий Павлович ", 2, 12000);
+        employees[2] = new Employee("Прохоров Сергей Петрович ", 3, 16000);
+        employees[3] = new Employee("Афонин Андрей Владимирович ", 1, 32000);
+        employees[4] = new Employee("Ангаборов Виталий Степановий ", 4, 45000);
+        employees[5] = new Employee("Коряков Владимир Ильич ", 5, 50000);
+        employees[6] = new Employee("Степанов Федор Евгеньевич ", 3, 40000);
+        employees[7] = new Employee("Калужников Эдуард Иванович ", 2, 16000);
+        employees[8] = new Employee("Сергеев Пётр Николаевич ", 5, 60000);
+        employees[9] = new Employee("Сидоров Арсений Андреевич ", 1, 73000);
+    }
+
+    public static void main(String[] args) {
+        initEmpoyees();
+        print();
+        System.out.println("Общая сумма потраченная на заработную плату = " + calculateSalary());
+        System.out.println("Сотрудник с минимальной заработной платой: " + minSalary());
+        System.out.println("Сотрудник с максимальной заработной платой: " + maxSalary());
+        System.out.println("Средняя заработная плата =  " + sredSalary());
+        printFullname();
+    }
+
+    private static void print() {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+
         }
+    }
+
+    private static int calculateSalary() {
+        int sum = 0;
+        for (Employee employee : employees) {
+            sum = sum + employee.getSalary();
+        }
+        return sum;
+    }
+
+    private static Employee minSalary() {
+        Employee employeeMinSalary = null;
+        for (Employee employee : employees) {
+            if (employeeMinSalary == null || employee.getSalary() < employeeMinSalary.getSalary()) {
+                employeeMinSalary = employee;
+            }
+        }
+        return employeeMinSalary;
+    }
+
+    private static Employee maxSalary() {
+        Employee employeeMaxSalary = null;
+        for (Employee employee : employees) {
+            if (employeeMaxSalary == null || employee.getSalary() > employeeMaxSalary.getSalary()) {
+                employeeMaxSalary = employee;
+            }
+        }
+        return employeeMaxSalary;
+    }
+
+    private static double sredSalary() {
+        return (double) calculateSalary() / employees.length;
+    }
+
+    private static void printFullname() {
+        for (Employee employee : employees) {
+            System.out.println(employee.getFullName());
+        }
+
     }
 }
